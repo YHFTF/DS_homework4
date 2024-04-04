10e0e5097fd1053b563e6634d7efe90e8f3ac6f0
YTFTF <yunghy1@chungbuk.ac.kr>
2024-04-04 18:38:44 +0900

    size.c 주석달기

--- size.c
+++ size.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int **x;
-    printf("sizeof(x) = %lu\n", sizeof(x));
-    printf("sizeof(*x) = %lu\n", sizeof(*x));
-    printf("sizeof(**x) = %lu\n", sizeof(**x));
+    int **x;                                        //이중 포인터 x를 선언한다. 
+    printf("sizeof(x) = %lu\n", sizeof(x));         //이중포인터 x의 크기를 출력한다.
+    printf("sizeof(*x) = %lu\n", sizeof(*x));       //이중포인터 x가 가리키는 주소의 크기를 출력한다.
+    printf("sizeof(**x) = %lu\n", sizeof(**x));     //이중포인터 x가 가리키는 주소의 값의 크기를 출력한다.
 }
